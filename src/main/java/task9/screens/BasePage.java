03aab7fba96b175b2532a1ad0d4968344cc8691b
Diana Luzgina <DianaLuzgina@gmail.com>
2020-07-30 11:42:34 +0300

    make method protected

--- src/main/java/task9/screens/BasePage.java
+++ src/main/java/task9/screens/BasePage.java
@@ -1,15 +1,13 @@
 package task9.screens;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import task9.entities.Browser;
 
 public class BasePage {
-    protected Browser browser;
 
-    public BasePage(){
-        browser = Browser.getInstance();
-    }
+  protected Browser browser;
+
+  protected BasePage() {
+    browser = Browser.getInstance();
+  }
 }
 
