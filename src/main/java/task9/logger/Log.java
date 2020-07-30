5383ea55c988637ba6f9762644e32d90a1ac9fff
Diana Luzgina <DianaLuzgina@gmail.com>
2020-07-30 11:43:03 +0300

    add lines, dell comments

--- src/main/java/task9/logger/Log.java
+++ src/main/java/task9/logger/Log.java
@@ -4,21 +4,22 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 
 public class Log {
-    private static Logger logger = Logger.getLogger("MyLogger");
 
-    public static void logInfo(String message){
-        logger.info(message);
-    }
+  private static Logger logger = Logger.getLogger("MyLogger");
 
-    public static void logDebug(String message){
-        logger.debug(message);
-    }
+  public static void logInfo(String message) {
+    logger.info(message);
+  }
 
-    public static void logClick(By by){
-        logger.debug(String.format("Click element located: [%s]", by));
-    }
+  public static void logDebug(String message) {
+    logger.debug(message);
+  }
 
-    public static void logTestFail(Throwable throwable){
-        logger.error(throwable.getMessage());
-    }
+  public static void logClick(By by) {
+    logger.debug(String.format("Click element located: [%s]", by));
+  }
+
+  public static void logTestFail(Throwable throwable) {
+    logger.error(throwable.getMessage());
+  }
 }
